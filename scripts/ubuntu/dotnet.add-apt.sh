6addb52871e4432f2e9537592f51a9401e13920b
dkowald <derek@kowald.id.au>
2017-05-29 10:26:46 +1000

    sample scripts to install dotnet

---
+++ scripts/ubuntu/dotnet.add-apt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Add dot net deb feed.
+# https://www.microsoft.com/net/core#linuxubuntu
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+sudo apt-get update
---
+++ scripts/ubuntu/dotnet.install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Add dot net deb feed.
+# https://www.microsoft.com/net/core#linuxubuntu
+
+sudo apt-get install dotnet-dev-1.0.4
